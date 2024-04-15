c1ed5c2c2ebc98c866688e43736f10aced1fba9d
shintia <shintiazhouu2@gmail.com>
2024-04-15 20:23:06 +0700

    fix: sidebar

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,15 @@
 <script setup>
-// import Header from '@/components/Header.vue'
-const publicRoutes = ['/']
+import Sidebar from '@/components/Sidebar.vue'
+const publicRoutes = ['/login']
 const isPublic = publicRoutes.includes(window.location.pathname)
 </script>
 
 <template>
-  <!-- <Header v-if="!isPublic" /> -->
-  <RouterView />
+  <div class="drawer md:drawer-open">
+    <input id="my-drawer-2" type="checkbox" class="drawer-toggle" />
+    <Sidebar v-if="!isPublic" />
+    <div class="drawer-content flex flex-col">
+      <RouterView />
+    </div>
+  </div>
 </template>
--- src/pages/Homepage.vue
+++ src/pages/Homepage.vue
@@ -1,23 +1,6 @@
 <template>
-  <div class="drawer md:drawer-open">
-    <input id="my-drawer-2" type="checkbox" class="drawer-toggle" />
-    <Sidebar />
-    <div class="drawer-content flex flex-col">
-      <!-- Page content here -->
-      <div class="bg-white min-h-full">
-        <label for="my-drawer-2" class="btn btn-primary drawer-button md:hidden">Open drawer</label>
-        <h1>homepage</h1>
-      </div>
-    </div>
+  <div class="bg-white min-h-full">
+    <label for="my-drawer-2" class="btn btn-primary drawer-button md:hidden">Open drawer</label>
+    <h1>homepage</h1>
   </div>
 </template>
-
-<script>
-import Sidebar from '@/components/Sidebar.vue'
-import { useRoute } from 'vue-router'
-export default {
-  components: {
-    Sidebar
-  }
-}
-</script>
