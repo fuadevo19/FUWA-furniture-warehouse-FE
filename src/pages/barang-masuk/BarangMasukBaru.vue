6c7fd1793520bc97f83996a131933026e1caa8e6
shintia <shintiazhouu2@gmail.com>
2024-04-16 13:49:30 +0700

    feta: create step2

--- src/pages/barang-masuk/BarangMasukBaru.vue
+++ src/pages/barang-masuk/BarangMasukBaru.vue
@@ -1,40 +1,30 @@
-<script></script>
+<script setup>
+import { ref } from 'vue'
+import Step1 from './Step1.vue'
+import Step2 from './Step2.vue'
+import Step3 from './Step3.vue'
+
+const currentStep = ref(0)
+
+const nextStep = () => {
+  if (currentStep.value < 2) {
+    currentStep.value++
+  }
+}
+</script>
 
 <template>
   <div class="bg-white min-h-full p-6 mt-10 max-w-5xl space-y-2">
     <h1 class="text-center text-2xl font-bold text-secondary">Masukkan Informasi Barang Masuk</h1>
-    <label class="form-control w-full max-w-xs">
-      <div class="label">
-        <span class="label-text">Tanggal dan Waktu</span>
+
+    <Step1 v-if="currentStep === 0" />
+    <Step2 v-else-if="currentStep === 1" />
+    <Step3 v-else-if="currentStep === 2" />
+
+    <div>
+      <div @click="nextStep" class="w-fit ml-auto">
+        <button class="btn btn-md btn-secondary mt-10">Selanjutnya</button>
       </div>
-      <input
-        type="text"
-        placeholder="Masukkan Tanggal dan Waktu"
-        class="input input-bordered w-full max-w-xs"
-      />
-    </label>
-    <label class="form-control w-full max-w-xs">
-      <div class="label">
-        <span class="label-text">Reference Number</span>
-      </div>
-      <input
-        type="text"
-        placeholder="Masukkan Reference Number"
-        class="input input-bordered w-full max-w-xs"
-      />
-    </label>
-    <label class="form-control w-full max-w-xs">
-      <div class="label">
-        <span class="label-text">Supplier ID</span>
-      </div>
-      <input
-        type="text"
-        placeholder="Masukkan Supplier ID"
-        class="input input-bordered w-full max-w-xs"
-      />
-    </label>
-    <div class="w-fit ml-auto">
-      <button class="btn btn-md btn-secondary mt-10">Selanjutnya</button>
     </div>
   </div>
 </template>
---
+++ src/pages/barang-masuk/Step2.vue
@@ -0,0 +1,41 @@
+<script></script>
+
+<template>
+  <div class="bg-white min-h-full p-4 mt-10 max-w-5xl space-y-4">
+    <section class="flex justify-between items-center">
+      <div>
+        <h2 class="font-bold text-lg">List Barang</h2>
+        <p>Masukkan list barang</p>
+      </div>
+      <button class="btn btn-sm btn-secondary">Tambah Barang +</button>
+    </section>
+    <div class="overflow-x-auto whitespace-nowrap">
+      <table class="table">
+        <!-- head -->
+        <thead>
+          <tr>
+            <th>No</th>
+            <th>Nama Barang</th>
+            <th>SKU</th>
+            <th>Ukuran</th>
+            <th>Berat</th>
+            <th>Qty</th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>
+          <!-- row 1 -->
+          <tr>
+            <td>1</td>
+            <td>Lemari 4 pintu</td>
+            <td>LLC123</td>
+            <td>60kcm x 20cm x 10cm</td>
+            <td>21Kg</td>
+            <td>3</td>
+            <td><button class="bg-red-200 text-red-700 px-2 rounded-full">Remove</button></td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </div>
+</template>
