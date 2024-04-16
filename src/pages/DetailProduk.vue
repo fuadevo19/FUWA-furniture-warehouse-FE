8025c698f99e8f0071ffd3ff3925583479f43c61
zettonnn <fajar25062002@gmail.com>
2024-04-16 11:54:09 +0700

    Detail Produk

---
+++ src/pages/DetailProduk.vue
@@ -0,0 +1,42 @@
+<script setup></script>
+
+<template>
+    <div>
+        <h1 class="text-4xl font-bold text-blue-500 mb-3 mt-3 text-center">Produk Page</h1>
+    </div>
+
+    <div>
+        <h2 class="text-xl ml-6 mt-10">Nama Barang</h2>
+        <input type="text" placeholder="Type here" class="input input-bordered w-1/2 ml-6 mt-2" />
+    </div>
+
+    <div>
+        <h2 class="text-xl ml-6 mt-6">Deskripsi Barang</h2>
+        <input type="text" placeholder="Type here" class="input input-bordered w-1/2 ml-6 mt-2" />
+    </div>
+
+    <div>
+        <h2 class="text-xl ml-6 mt-6">SKU</h2>
+        <input type="text" placeholder="Type here" class="input input-bordered w-1/2 ml-6 mt-2" />
+    </div>
+
+    <div>
+        <h2 class="text-xl ml-6 mt-6">Berat</h2>
+        <input type="text" placeholder="Type here" class="input input-bordered w-1/2 ml-6 mt-2" />
+    </div>
+
+    <div>
+        <h2 class="text-xl ml-6 mt-6">Ukuran</h2>
+        <input type="text" placeholder="Type here" class="input input-bordered w-1/2 ml-6 mt-2" />
+    </div>
+
+    <div>
+        <h2 class="text-xl ml-6 mt-6">Zone</h2>
+        <input type="text" placeholder="Type here" class="input input-bordered w-1/2 ml-6 mt-2" />
+    </div>
+
+    <div class="mt-6">
+        <button class="btn btn-info float-right mr-20 w-64 mt-10">SIMPAN</button>      
+    </div>
+      
+</template>
--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import Homepage from '@/pages/Homepage.vue'
 import Login from '@/pages/Login.vue'
 import PageProduk from '@/pages/PageProduk.vue'
+import DetailProduk from '@/pages/DetailProduk.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -20,6 +21,11 @@ const router = createRouter({
       path: '/',
       name: 'home',
       component: Homepage
+    },
+    {
+      path: '/detailproduk',
+      name: 'detailproduk',
+      component: DetailProduk
     }
   ]
 })
