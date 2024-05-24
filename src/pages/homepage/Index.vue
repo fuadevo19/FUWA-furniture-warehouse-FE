9c44ce42ff806c403795c4627144974d1843f7e8
shintia <shintia.zhou@topremit.com>
2024-05-24 16:41:55 +0700

    chore: fix by remove

--- src/pages/homepage/Index.vue
+++
@@ -1,6 +0,0 @@
-<template>
-  <div class="bg-white min-h-full">
-    <label for="my-drawer-2" class="btn btn-primary drawer-button md:hidden">Open drawer</label>
-    <h1>homepage</h1>
-  </div>
-</template>
