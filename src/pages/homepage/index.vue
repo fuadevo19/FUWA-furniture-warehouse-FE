ae476751c73191139997187c208754354d5d5c5c
shintia <shintia.zhou@topremit.com>
2024-05-24 16:42:36 +0700

    chore: fix index

---
+++ src/pages/homepage/index.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="bg-white min-h-full">
+    <label for="my-drawer-2" class="btn btn-primary drawer-button md:hidden">Open drawer</label>
+    <h1>homepage</h1>
+  </div>
+</template>
