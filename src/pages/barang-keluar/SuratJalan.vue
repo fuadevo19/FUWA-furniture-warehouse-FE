55b8c80a186c516cb9787ceb2925133235475d9c
dhenmasadhi <dhenmasa@gmail.com>
2024-06-16 13:28:51 +0700

    membuat surat jalan

--- src/pages/barang-keluar/SuratJalan.vue
+++ src/pages/barang-keluar/SuratJalan.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="bg-white min-h-full px-9">
+  <div class="bg-white min-h-full px-9" v-if="outboundDetail">
     <div class="flex justify-between mt-6">
       <div>
         <img class="h-[70px] w-[75px]" :src="logoIcon" alt="fuwa-ms" />
@@ -10,7 +10,7 @@
       </div>
       <div class="flex flex-col justify-center items-center">
         <h1 class="font-bold text-xl">SURAT JALAN</h1>
-        <h3>ID : 123213123</h3>
+        <h3>ID : {{ outboundDetail.id }}</h3>
       </div>
     </div>
     <hr class="border-black my-4" />
@@ -20,17 +20,17 @@
         <li>Nomor Telepon</li>
         <li>Alamat</li>
       </ul>
-      <ul class="space-y-2">
-        <li>shintia zhou</li>
-        <li>0812312312</li>
-        <li>Tanah Abang Jakarta</li>
+      <ul class="space-y-2 font-semibold">
+        <li>{{ outboundDetail.customer_name }}</li>
+        <li>{{ outboundDetail.customer_number }}</li>
+        <li>{{ outboundDetail.customer_address }}</li>
       </ul>
       <ul class="space-y-2">
         <li>Tanggal Kirim</li>
         <li>Tanggal diterima</li>
       </ul>
-      <ul class="space-y-2">
-        <li>12 Juni 2024</li>
+      <ul class="space-y-2 font-semibold">
+        <li>{{ formatDate(outboundDetail.datetime) }}</li>
         <li><hr class="mt-7 border border-black" /></li>
       </ul>
     </div>
@@ -47,12 +47,12 @@
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <th>1</th>
-            <td>Lemari laci 4 pintu putih</td>
-            <td>60cm x 39.5cm x 68cm</td>
-            <td>21Kg</td>
-            <td>3</td>
+          <tr v-for="(product, index) in outboundDetail.OutboundProducts" :key="product.id">
+            <th>{{ index + 1 }}</th>
+            <td>{{ product.Product.name }}</td>
+            <td>{{ product.Product.size }}</td>
+            <td>{{ product.Product.weight }}Kg</td>
+            <td>{{ product.quantity }}</td>
           </tr>
         </tbody>
       </table>
@@ -62,20 +62,62 @@
       <div class="border-t border-black px-10">Pengirim</div>
       <div class="border-t border-black px-10">Penerima</div>
     </div>
-    <div class="w-fit ml-auto">
-      <button class="btn btn-md rounded-md btn-secondary px-8">Print</button>
+    <div class="w-fit ml-auto flex space-x-4 mt-4 print:hidden">
+      <button class="btn btn-md rounded-md btn-secondary px-8" @click="printPage">Print</button>
+      <button class="btn btn-md rounded-md btn-secondary px-8" @click="completeDelivery">
+        Selesai
+      </button>
     </div>
+  </div>
+  <div v-else>
+    <p>Loading...</p>
   </div>
 </template>
 
 <script>
+import { format } from 'date-fns'
+import { outbound } from '@/queries/outbound'
 import logoIcon from '@/assets/logo_icon_dark.png'
 
 export default {
   data() {
     return {
+      outboundDetail: null,
       logoIcon
     }
+  },
+  methods: {
+    formatDate(dateStr) {
+      const date = new Date(dateStr)
+      return format(date, 'dd MMMM yyyy HH:mm:ss')
+    },
+    async fetchOutboundDetails() {
+      const outboundId = this.$route.query.id
+      try {
+        const data = await outbound.getById(outboundId)
+        this.outboundDetail = {
+          ...data,
+          datetime: this.formatDate(data.datetime)
+        }
+      } catch (error) {
+        console.error('Error fetching outbound details:', error)
+      }
+    },
+    printPage() {
+      window.print()
+    },
+    async completeDelivery() {
+      try {
+        await outbound.updateStatus(this.outboundDetail.id, { status: 'On Delivery' })
+        this.$router.push('/barang-keluar')
+      } catch (error) {
+        console.error('Error updating status:', error)
+        alert('Failed to update status')
+      }
+    }
+  },
+  mounted() {
+    this.fetchOutboundDetails()
   }
 }
 </script>
